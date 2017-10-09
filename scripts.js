bbcf1e02283e9f835adb84ca83d93c780b520f7d
Magda Krawczyk <magdalena.a.krawczyk@gmail.com>
2017-10-09 14:10:25 +0200

    Zadanie 10-2-v2

--- scripts.js
+++ scripts.js
@@ -1,27 +1,17 @@
 $(document).ready(function(){
-	console.log('DOM loaded - you can have fun');
-});
 
-// var span = $("span");
-//     span.each(function(index, element) {
-// });
-//
-// span.each(function(index, element) {
-// 	if(index % 2 == 0) {
-// 		$(element).css('color', 'red');
-// 	};
-// });
+  $("span:even").css('color', 'green');
 
-$("span:even").css('color', 'green');
+  var paragraphs = $('p');
+  paragraphs.each(function(index, element) {
 
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
+      var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+      $(element).append(button)
 
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
+  });
 
-});
+  $("button").click(function(){
+  	alert($(this).attr("data-tmp"));
+  });
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
 });
